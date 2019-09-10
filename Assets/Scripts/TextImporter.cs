031fc401f48395045a921ecd56cbb092bd5bac86
Daniel Lind <dlind777@gmail.com>
2019-09-09 16:56:37 -0800

    Project added

---
+++ Assets/Scripts/TextImporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextImporter : MonoBehaviour
+{
+    public TextAsset textFile;
+    public string[] dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(textFile != null)
+        {
+            dialogue = (textFile.text.Split('\n'));
+        }
+    }
+}
