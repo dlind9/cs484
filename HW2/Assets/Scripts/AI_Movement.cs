9eaaa898fb400122d078caf83822d4b4639b21fa
Daniel Lind <dlind777@gmail.com>
2019-09-15 17:46:45 -0800

    Add basic working build

---
+++ HW2/Assets/Scripts/AI_Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Movement : MonoBehaviour
+{
+    public Animator doge;
+    public int rand;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rand = Random.Range(0, 3);
+        doge.SetFloat("chooseDirection", rand);
+    }
+}
---
+++ HW2/Assets/Scripts/barMeterScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class barMeterScript : MonoBehaviour
+{
+    public int val;
+    public int decVal;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // HungerBar.UpdateBar(100, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value -= decVal * Time.deltaTime;
+    }
+
+    public void updateBar()
+    {
+        slider.value += val;
+    }
+}
