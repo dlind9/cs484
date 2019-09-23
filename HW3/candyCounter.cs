f44095faa3dcf5b3c0bdb9e272d4485d12780e63
Daniel Lind <dlind777@gmail.com>
2019-09-22 19:53:33 -0800

    Incomplete HW3 added, no escape or start yet

---
+++ HW3/candyCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class candyCounter : MonoBehaviour
+{
+    private int maxCandy;
+    public int currentCandy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxCandy = 4;
+        currentCandy = 0;
+    }
+
+    public void candyGrabbed()
+    {
+        if (currentCandy < 4)
+        {
+            currentCandy++;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
---
+++ HW3/candyGrabbed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class candyGrabbed : MonoBehaviour
+{
+    public candyCounter candyCounter;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        candyCounter.candyGrabbed();
+        Destroy(this.gameObject);
+        Debug.Log("Candy grabbed");
+    }
+}
